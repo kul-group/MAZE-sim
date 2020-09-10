8fb508a3fe0c4b799b1f126dcc8bf79ca6cf67e4
sjholton <sjholton@ucdavis.edu>
2020-09-10 15:40:57 -0700

    separate get_cluster fxn

--- source/get_cluster.py
+++ source/get_cluster.py
@@ -5,10 +5,10 @@ from ase.io import read, write
 
 # Obtains the indicies of a cluster in a zeolite given center atom and cluster size
 
-def get_cluster(atoms, index, size)
+def get_cluster(atoms, index, size):
     '''
     :param atoms: atoms object 
-    :param size: number of T-sites in cluster, integer
+    :param size: number of atoms in cluster (not number of T-sites!), integer
     :param index: index at center of cluster, integer
     :return: list of indicies included in the cluster
     '''
@@ -28,7 +28,20 @@ def get_cluster(atoms, index, size)
     atoms_cluster.append(atoms[index])
     atoms.set_tags(0)
     atoms[index].tag = 1
+    clust_ind = []
 
+    # uses tagging system to find the cluster
+    while len(clust_ind) < size:
+        for i in atoms:
+            if i.tag == 1:
+                adj = nl.get_neighbors(i.index)[0]
+                i.tag = 2  # change tag so atom not expanded in future
+                for j in adj:
+                    if atoms[j].tag == 0:
+                        clust_ind.append(j)
+                        atoms[j].tag = 1 # neighbors of newly added atoms will be expanded next
+
+    return(clust_ind)
 
 
 
@@ -36,4 +49,5 @@ def get_cluster(atoms, index, size)
 if __name__ == '__main__':
     from ase.io import read
     b = read('BEA.cif')
-    a = get_cluster(b, 100, 5)
+    a = get_cluster(b, 101, 5)
+    print(a)
