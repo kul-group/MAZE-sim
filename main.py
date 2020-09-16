4dd6deece39924d0a5159d5dd21fc419b02ba047
Dexter Antonio <dexter.d.antonio@gmail.com>
2020-09-16 12:06:59 -0700

    cleaned up main function

--- main.py
+++ main.py
@@ -7,59 +7,6 @@ import copy
 from ase.io import cif
 import ase
 
-def my_read_cif(fileobj, store_tags=False, primitive_cell=False,
-             subtrans_included=True, fractional_occupancies=True,
-             reader='ase'):
-
-    blocks = ase.io.cif.parse_cif(fileobj, reader)
-    tags = blocks[0][1]
-    # Find all CIF blocks with valid crystal data
-    atoms = ase.io.cif.tags2atoms(tags, store_tags, primitive_cell,
-                               subtrans_included,
-                               fractional_occupancies=fractional_occupancies)
-
-    return atoms
-    #         if store_tags:
-    #             try:
-    #                 atoms.set_tags(atoms.info['_atom_Site_type_symbol'])
-    #             except KeyError:
-    #                 pass
-    #         return atoms
-    #         images.append(atoms)
-    #     except KeyError:
-    #         pass
-    # for atoms in images[0]:
-    #     yield atoms
-
-
-def my_read_cif_2(fileobj, store_tags=False, primitive_cell=False,
-             subtrans_included=True, fractional_occupancies=True,
-             reader='ase'):
-    blocks = ase.io.cif.parse_cif(fileobj, reader)
-    # Find all CIF blocks with valid crystal data
-    images = []
-    for name, tags in blocks:
-        try:
-            atoms = ase.io.cif.tags2atoms(tags, store_tags, primitive_cell,
-                               subtrans_included,
-                               fractional_occupancies=fractional_occupancies)
-            images.append(atoms)
-        except KeyError:
-            pass
-    for atoms in images:
-        yield atoms
-
-def build_zeolite_from_cif(fileobj):
-    atoms_gen = my_read_cif_2(fileobj, store_tags=True)
-    #atoms = my_read_cif_2(fileobj, store_tags=True) # next(atoms_gen) # this might be good enough
-    atoms = next(atoms_gen)
-    my_zeotype = Zeotype(atoms)
-    try:
-        my_zeotype.atom_sites_label = atoms.info['_atom_site_label']
-    except KeyError:
-        print("No atom site labels in CIF")
-        pass
-    return my_zeotype
 
 if __name__ == '__main__':
     cif_path = os.path.join(os.getcwd(), 'source', 'BEA.cif')
