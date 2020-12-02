e2f42111a9b65ae64f3db8bf15ea7e113d9320dc
Dexter Antonio <dexter.d.antonio@gmail.com>
2020-12-02 15:12:48 -0800

    code cleaned up

--- demo/sam.py
+++ demo/sam.py
@@ -2,16 +2,14 @@ from ase.io import read, write
 from source.zeotype import Zeotype
 import os
 from ase.visualize import view
+from pathlib import Path
 
 def main():
-    #data_dir = os.path.join(os.getcwd(), 'data', 'sam')
-    #filename = "sn_ti-8T-cluster.traj"
-    # filepath = os.path.join(data_dir, filename)
-    data_dir = '/Users/dda/Code/zeotype/data'
-    filepath1 = "/Users/dda/Code/zeotype/data/sam/sn_ti-8T-cluster.traj"
-    filepath2 = "/Users/dda/Code/zeotype/data/sam/sn_ti-periodic.traj"
-    view(read(filepath1))
-    z = Zeotype(read(filepath2))
+    data_dir = os.path.join(Path(os.getcwd()).parent, 'data', 'sam')
+
+    sn_ti_traj_filepath = os.path.join(data_dir, "sn_ti-periodic.traj")
+    z = Zeotype(read(sn_ti_traj_filepath))
+
     cluster_indices = [191,
                        168, 120, 84, 96, 112,
                        159, 79, 63, 87, 35,
@@ -26,13 +24,8 @@ def main():
     z.clusters[cluster_index].cap_atoms()
     view(z.clusters[cluster_index])
     view(z)
-    print(z.clusters[cluster_index].zeotype_to_cluster_index_map)
-    write(os.path.join(data_dir, 'dexter_t8_cluster.traj'), z.clusters[cluster_index])
+    write(os.path.join(data_dir, 'sam_t8_cluster.traj'), z.clusters[cluster_index])
 
-    # z = Zeotype(a)
-    # z2 = Zeotype(read(filepath2))
-    # view(z2)
-    # view(z)
 
 
 if __name__ == "__main__":
