54979fa7e8a9d4e4577c672b263f6c7f9c23cfcb
Dexter Antonio <dexter.d.antonio@gmail.com>
2020-12-08 16:20:42 -0800

    fixed bug and added demo file

--- demo/demo_add_si_nest.py
+++ demo/demo_add_si_nest.py
@@ -1,8 +1,10 @@
+import os
 from source.zeotype import Zeotype
 from ase.visualize import view
-
+from patlib import path
 def main():
-    cif_dir = "/Users/dda/Code/zeotype/data/GOO.cif"
+    data_dir = os.path.join(Path(os.getcwd()).parent, 'data')
+    cif_dir = os.path.join(data_dir, 'GOO.cif')
     zeolite = Zeotype.build_from_cif_with_labels(cif_dir)
     T1_index = 95
     zeolite[T1_index].symbol = 'Hg'
