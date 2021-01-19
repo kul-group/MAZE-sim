3005a3d7f31f1a050ae9caff109de9dac48a37ee
Dexter Antonio <dexter.d.antonio@gmail.com>
2021-01-19 12:26:12 -0800

    modified maze

--- maze/__init__.py
+++ maze/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['adsorbate', 'zeotype', 'index_mapper', 'cif_download']
-from maze import adsorbate, zeotype, index_mapper, cif_download
+from maze import adsorbate, zeotype, cif_download, index_mapper
