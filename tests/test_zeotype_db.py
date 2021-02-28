d3e10fca8d51587160da98bbcc8a6d854bbb0d10
Dexter Antonio <dexter.d.antonio@gmail.com>
2021-02-28 15:53:30 -0800

    added test cases for db

---
+++ tests/test_zeotype_db.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+
+class TestZeotypeDatabase(TestCase):
+    def test_add_data(self):
+        self.fail()
+
+    def test__get(self):
+        self.fail()
+
+    def test__write(self):
+        self.fail()
+
+    def test_write_data(self):
+        self.fail()
+
+    def test_make_parent_dict(self):
+        self.fail()
+
+    def test_write_parent(self):
+        self.fail()
+
+    def test_update_parent(self):
+        self.fail()
+
+    def test_write(self):
+        self.fail()
+
+    def test_build_parent_zeotype(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
