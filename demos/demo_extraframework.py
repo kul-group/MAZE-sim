f154f0603ba6e21f5262b93595c13a1be439cd20
JiaweiGuo-0313 <63579283+JiaweiGuo-0313@users.noreply.github.com>
2021-04-03 14:33:11 -0700

    Update demo_extraframework.py

--- demos/demo_extraframework.py
+++ demos/demo_extraframework.py
@@ -1,22 +1,41 @@
-from maze.extra_framework_maker import ExtraFramework
+from maze.extra_framework_maker import ExtraFrameworkMaker
 from ase.io import write, read
 from ase.visualize import view
+from ase import Atoms
 
 
 def main():
-    cif_dir = 'MFI.cif'
-    my_zeolite = ExtraFramework(cif_dir=cif_dir)
-    # unique_t_site_indices = my_zeolite.get_unique_t_sites(cif_dir)
-    my_zeolite.create_1Al_replacement()
-    my_zeolite.create_2Al_replacement()
-    # write('MFI_2Al_replaced.traj', my_zeolite.traj_2Al)
-    print(my_zeolite.count)
+    sample_zeolite = 'AEI'
+    EFzeolite = ExtraFrameworkMaker(iza_code=sample_zeolite)
+    # 1Al and 2Al replacement
+    EFzeolite.make_extra_frameworks(print_statement=True)
+    print(EFzeolite.t_site_indices)
+    # print(len(EFzeolite.traj_1Al))
+    print('Number of Unique Al pairs: ', len(EFzeolite.traj_2Al))
+    # view(EFzeolite.traj_1Al)
+    # view(EFzeolite.traj_2Al)
 
-    traj = read('MFI_2Al_replaced.traj', ':')
-    traj_CuOCu = []
-    for atoms in traj:
-        traj_CuOCu.append(my_zeolite.insert_CuOCu(atoms))
-    view(traj_CuOCu)
+    # extra-framework insertion (extra-framework atoms input can be ASE Atoms object or user-defined traj files)
+    zeolite = EFzeolite.traj_2Al[0]
+    EF_atoms = Atoms('Co', positions=[[0, 0, 0]])
+    # EF_atoms = read('/Users/jiaweiguo/Desktop/MAZE-sim-master/demos/CuOCu_cluster.traj', '0')
+    inserted_atoms = EFzeolite.insert_ExtraFrameworkAtoms(zeolite, EF_atoms)
+    view(inserted_atoms)
+    print('Extra-framework atoms insertion is done!')
+
+    # sample all Z-TM sites
+    dict_Z_TM = EFzeolite.get_all_Z_TM(d_Z_TM=2.6, TM_type='Cu')  # Al-Cu distance
+    all_traj = []
+    for site_name, traj in dict_Z_TM.items():
+        for each_traj in traj:
+            all_traj.append(each_traj)
+    view(all_traj)
+    print('All Z-TM site sampling is done!')
+
+    # sample all Bronsted sites
+    traj_ZH = EFzeolite.get_all_Bronsted_sites()
+    view(traj_ZH)
+    print('All Bronsted site sampling is done!')
 
 
 if __name__ == '__main__':
