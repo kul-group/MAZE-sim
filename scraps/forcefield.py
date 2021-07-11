c8f9a68ac3fd71bdcc195dd5e07dcec9aeba1189
Jiawei Guo <jwguo@ucdavis.edu>
2021-07-10 23:01:23 -0700

    added doc_strings, updated bond track function

--- scraps/forcefield.py
+++ scraps/forcefield.py
@@ -16,35 +16,57 @@ from simtk.unit import *
 from sys import stdout
 from ase.geometry.analysis import Analysis
 import numpy as np
+from itertools import permutations
 
 
 def get_capped_cluster(atoms, file_name):
+    """
+    Extract smaller cluster containing the extra-framework atoms and then cap all the O.
+    Save cluster in both .traj file and .pdb format.
+    :param :
+    :param :
+    :return:
+    """
     EFMaker = ExtraFrameworkAnalyzer(atoms)
     cluster = atoms[[index for index in EFMaker.get_extraframework_cluster()]]
     cluster = Zeolite(cluster).cap_atoms()
 
     write('/Users/jiaweiguo/Box/openMM_test/%s.traj' %file_name, cluster)
     proteindatabank.write_proteindatabank('/Users/jiaweiguo/Box/openMM_test/%s.pdb' %file_name, cluster)
-    # pdb file labeling needs to be streamlined
     # gromacs.write_gromacs('/Users/jiaweiguo/Box/cluster.gro', cluster)
     return cluster
 
 
-def get_all_bonds():
-    cluster = read('/Users/jiaweiguo/Box/cluster.traj', '0')
+def get_bonds(cluster, excluded_index=None, excluded_pair=None):
+    """
+    Using ase.geometry.analysis.Analysis to get all bonds, then remove the repeated ones.
+    Function also allows removing certain bonding pair defined by user (excluede_pair).
+    Or removing pairs including certain atomic indices (excluded_index).
+    :param excluded_index: list of integers
+    :param excluded_pair: list of lists
+    :return: full bonding list, shortened list.
+    If both excluded_index and excluded_pair are None, bonding list == shortened list
+    """
+    if excluded_index is None:
+        excluded_index = []
+    if excluded_pair is None:
+        excluded_pair = []
+
     nl = NeighborList(natural_cutoffs(cluster), bothways=True, self_interaction=False)
     nl.update(cluster)
 
-    bond_list, bond_list_wo_H = [], []
-    index_H = [a.index for a in cluster if a.symbol == 'H']
+    bond_list, shortened_list = [], []
     for count, indices in enumerate(Analysis(cluster, nl=nl).all_bonds[0]):
         for index in indices:
             if [count, index] not in bond_list and [index, count] not in bond_list:
                 bond_list.append([count, index])
-                if count not in index_H and index not in index_H:
-                    bond_list_wo_H.append([count, index])
 
-    return bond_list, bond_list_wo_H
+    for bond in bond_list:
+        if all(single_index not in bond for single_index in excluded_index) and \
+                all(tuple(bond) not in list(permutations(pair)) for pair in excluded_pair):
+            shortened_list.append(bond)
+
+    return bond_list, shortened_list
 
 
 def label_pdb(file_name):
@@ -58,7 +80,7 @@ def label_pdb(file_name):
             name_list.append(name)
             name = name + str(name_list.count(name))
             name = name.rjust(4)
-            line = line.replace(line[12:16], name, 1)
+            line = line.replace(line[12:16], name, 1)  # not replacing the symbol column
         fileout.writelines(line)
 
     filein.close()
@@ -79,30 +101,56 @@ def some_random_stuff():
     """
 
 
-if __name__ == '__main__':
-
-    # print(get_all_bonds())
-    """
+def func():
     traj = read('/Users/jiaweiguo/Box/MFI_minE_O_less.traj', ':')
     cluster_traj = []
     for count, atoms in enumerate(traj):
-        cluster_traj.append(get_capped_cluster(atoms, 'cluster_'+str(count)))
+        cluster_traj.append(get_capped_cluster(atoms, 'cluster_' + str(count)))
     view(cluster_traj)
-    """
 
     traj = read('/Users/jiaweiguo/Box/MFI_minE_O_less.traj', ':')
     for val in range(len(traj)):
         label_pdb('cluster_%s' % str(val))
 
 
+if __name__ == '__main__':
+    # print(get_all_bonds())
+    # func()
 
+    cluster = read('/Users/jiaweiguo/Box/openMM_test/cluster_0.traj', '0')
+    bond_list, shortened_list = get_bonds(cluster)
+    print(shortened_list == bond_list)
+
+    bond_list, shortened_list = get_bonds(cluster, excluded_index=[2, 3, 8, 9],
+                                          excluded_pair=[[11, 12], [0, 4], [0, 6], [1, 5], [1, 7]])
+    # for simplicity, excluding all O-H and Al-O bonds
+    print(shortened_list)
+
+
+    """
+    nl = NeighborList(natural_cutoffs(cluster), bothways=True, self_interaction=False)
+    nl.update(cluster)
+
+    print(Analysis(cluster, nl=nl).all_angles[0][0])
+"""
+
+
+    """
+    angle_list = []
+    for count, indices in enumerate(Analysis(cluster, nl=nl).all_angles[0]):
+        for index in indices:
+            if all(list(val) not in angle_list for val in list(permutations([count, index[0], index[1]]))):
+                angle_list.append([count, index[0], index[1]])
+    print(angle_list)
+    """
 
 
     """
     bond_list = [[0, 11], [1, 12], [4, 11], [5, 12], [6, 11], [7, 12], [10, 11], [10, 12]]
-    # shortened, excluding O-H and Al-OH
+    # shortened, excluding O-H and Al-OH for now
 
-    pdb = PDBFile('/Users/jiaweiguo/Box/cluster_labeled.pdb')
+    numb = 0
+    pdb = PDBFile('/Users/jiaweiguo/Box/openMM_test/cluster_%s_labeled.pdb' % numb)
     atoms = list(pdb.topology.atoms())
     print([atom.name for atom in atoms])
 
@@ -111,7 +159,8 @@ if __name__ == '__main__':
     bonds = list(pdb.topology.bonds())
     print([[bond[0].name, bond[1].name] for bond in bonds])
 
-    FF = ForceField('/Users/jiaweiguo/Box/template_v2.xml')   # v2 - no harmonic force functions in xml
+    FF = ForceField('/Users/jiaweiguo/Box/openMM_test/template_v2.xml')
+    # v2.xml removed all force functions in xml
     system = FF.createSystem(pdb.topology)
 
     force = CustomBondForce("D*(1-exp(-alpha*(r-r0)))^2")  # Morse bond
@@ -120,9 +169,8 @@ if __name__ == '__main__':
     force.addPerBondParameter("r0")
     for index in bond_list:
         force.addBond(index[0], index[1], [1, 1, 2])
-    print(force.getNumBonds())
+    # print(force.getNumBonds())
     system.addForce(force)
-
     integrator = LangevinMiddleIntegrator(300 * kelvin, 1 / picosecond, 0.004 * picoseconds)
     simulation = Simulation(pdb.topology, system, integrator)
     simulation.context.setPositions(pdb.positions)
