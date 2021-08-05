ca1b53d6f40aeb9254adcb4e127f88118004e216
Jiawei Guo <jwguo@ucdavis.edu>
2021-08-04 21:47:26 -0700

    Update EFAnalyzer.py

--- scraps/EFAnalyzer.py
+++ scraps/EFAnalyzer.py
@@ -10,6 +10,7 @@ from ase.visualize import view
 import copy as copy
 from ase.io import write, read
 import itertools
+from statistics import mode
 
 
 class ExtraFrameworkAnalyzer(object):
@@ -19,22 +20,25 @@ class ExtraFrameworkAnalyzer(object):
         self.dict_EF = {}
         self.atoms = PerfectZeolite(atoms)
 
-    def get_extraframework_atoms(self):
+    def get_extraframework_cluster(self, predefined_centering_o=None):
         """
         extract extra-framework cluster including Cu-O-Cu, 2 Al, and 8 O around the Als (13 atoms total)
+        :param predefined_centering_o: get the mode of all possible centering O index by training a bunch of
+        configurations for the same zeolite, to remove bias.
         """
         index_EF_TM = [a.index for a in self.atoms if a.symbol in self.TM_list]
         index_Al = [a.index for a in self.atoms if a.symbol == 'Al']
 
-        TM_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_EF_TM[0])[0],
-                                        self.atoms.neighbor_list.get_neighbors(index_EF_TM[1])[0]))
-
-        centering_o = [[x for x in TM_neigh_list if list(TM_neigh_list).count(x) > 1][0]]
-        # might not always return the correct atom index
-        
         Al_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_Al[0])[0],
                                         self.atoms.neighbor_list.get_neighbors(index_Al[1])[0]))
         Al_neigh_list = [x for x in Al_neigh_list if self.atoms[x].symbol == 'O']
+
+        if predefined_centering_o is not None:
+            centering_o = copy.copy(predefined_centering_o)
+        else:
+            TM_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_EF_TM[0])[0],
+                                            self.atoms.neighbor_list.get_neighbors(index_EF_TM[1])[0]))
+            centering_o = [[x for x in TM_neigh_list if list(TM_neigh_list).count(x) > 1 and x not in Al_neigh_list][0]]
 
         assert len(index_EF_TM) == 2
         assert len(index_Al) == 2
@@ -44,20 +48,28 @@ class ExtraFrameworkAnalyzer(object):
         return Al_neigh_list + index_Al + index_EF_TM + centering_o
 
 
+def check_centering_o_index():
+    zeo_list = ['MWW', 'CHA', 'AEI', 'BEA', 'LTA', 'MAZ', 'MFI', 'MOR', 'RHO', 'SOD']
+
+    for zeolite in zeo_list:
+        traj = read('/Users/jiaweiguo/Box/openMM_FF/%s_minE.traj' % zeolite, ':')
+        # view(traj)
+        EF_O_index, count = [], 0
+        for atoms in traj:
+            try:
+                EFAnalyzer = ExtraFrameworkAnalyzer(atoms)
+                EF_O_index.append(EFAnalyzer.get_extraframework_cluster()[-1])
+                count += 1
+            except:
+                ...
+        unique_index = np.unique(EF_O_index)
+        print(zeolite, count, list(unique_index),
+              [EF_O_index.count(unique_index[count]) for count in range(len(unique_index))])
+        # index check, if len(np.unique(EF_O_index)) > 1, remove
+
+
 if __name__ == '__main__':
-    # traj = read('/Users/jiaweiguo/Box/openMM_FF/CHA_minE.traj', ':')
-    # traj = read('/Users/jiaweiguo/Box/openMM_FF/AEI_minE.traj', ':')
-    # traj = read('/Users/jiaweiguo/Box/openMM_FF/RHO_minE.traj', ':')
-    traj = read('/Users/jiaweiguo/Box/openMM_FF/MOR_minE.traj', ':')
-
-
-    # view(traj)
-    EF_O_index = []
-    for atoms in traj:
-        EFAnalyzer = ExtraFrameworkAnalyzer(atoms)
-        EF_O_index.append(EFAnalyzer.get_extraframework_atoms()[-1])
-    print(np.unique(EF_O_index))
-    print(EF_O_index.count(96))
-    print(EF_O_index.count(33))
-    # index check, if len(np.unique(EF_O_index)) > 1, remove
+    atoms = read('/Users/jiaweiguo/Box/openMM_FF/%s_minE.traj' %'CHA', '0')
+    EFAnalyzer = ExtraFrameworkAnalyzer(atoms)
+    print(EFAnalyzer.get_extraframework_cluster([144])[-1])
     
