b9f7d7680a77744f353f392736f51b48be647a71
Jiawei Guo <jwguo@ucdavis.edu>
2021-08-04 17:01:35 -0700

    Update EFAnalyzer.py

--- scraps/EFAnalyzer.py
+++ scraps/EFAnalyzer.py
@@ -19,87 +19,37 @@ class ExtraFrameworkAnalyzer(object):
         self.dict_EF = {}
         self.atoms = PerfectZeolite(atoms)
 
-    @property
-    def EF_bond_vectors(self):
-        return self.get_all_bonds()
-
-    @property
-    def EF_angles(self):
-        return self.get_all_angles()
-
-    @staticmethod
-    def recentering_atoms(atoms, pos_to_center):
-        """ This function recenters the atoms object by translating the the input position "pos_to_center" to the center
-        of the cell.
-        :param atoms: zeolite backbone with two Al inserted
-        :param pos_to_center: some positions to be centered
-        :return: recentered atoms object, translation vector
-        """
-        vec_translate = np.matmul([0.5, 0.5, 0.5], atoms.get_cell()) - pos_to_center
-        atoms.translate(vec_translate)
-        atoms.wrap()
-        return atoms, vec_translate
-
-    """
     def get_extraframework_atoms(self):
-        #TODO: MORE GENERAL EXTRA FRAMEWORK DETECTION (CUFRRENTLY LIMITED TO TM-O-TM)
-
+        """
+        extract extra-framework cluster including Cu-O-Cu, 2 Al, and 8 O around the Als (13 atoms total)
+        """
         index_EF_TM = [a.index for a in self.atoms if a.symbol in self.TM_list]
         index_Al = [a.index for a in self.atoms if a.symbol == 'Al']
-        assert len(index_EF_TM) == 2
-        assert len(index_Al) == 2
-
-        # self.atoms.update_nl(1.2) need larger cutoff for tracking ZOCu oxygens
-
+        
         TM_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_EF_TM[0])[0],
                                         self.atoms.neighbor_list.get_neighbors(index_EF_TM[1])[0]))
-        Al_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_Al[0])[0],
-                                        self.atoms.neighbor_list.get_neighbors(index_Al[1])[0]))
 
-        # print(TM_neigh_list, Al_neigh_list)
-
-        # This is wrong! Not always return desired O index!
+        # This might be wrong! Not always return desired O index!
+        # works for All CHA structures at least
         centering_o = [[x for x in TM_neigh_list if list(TM_neigh_list).count(x) > 1][0]]
         # print(centering_o)
 
-        o_between_T_Cu = [val for val in TM_neigh_list if val in Al_neigh_list and self.atoms[val].symbol == 'O']
-        # print(o_between_T_Cu)
-
-        self.centering_atom_index = centering_o[0]
-        assert len(centering_o) == 1
-        assert self.atoms[centering_o].symbols == 'O'
-
-        EF_indices = [index_EF_TM]
-        EF_indices.extend(centering_o)
-        EF_symbols = [self.atoms[index_EF_TM[0]].symbol]
-        EF_symbols.extend('O')
-
-        self.EF_indices = list(centering_o)
-        self.EF_indices.extend([value for value in index_EF_TM])
-
-        for count, index in enumerate(EF_indices):
-            self.dict_EF_atoms[EF_symbols[count]] = index
-
-        self.o_between_T_Cu = o_between_T_Cu
-        # self.dict_EF_atoms['OZ'] = self.o_between_T_Cu   
-    """
-    def get_extraframework_cluster(self):
-        # extraframework atoms, 2 Al and surrounding 8 O
-        index_EF_TM = [a.index for a in self.atoms if a.symbol in self.TM_list]
-        index_Al = [a.index for a in self.atoms if a.symbol == 'Al']
-        assert len(index_EF_TM) == 2
-        assert len(index_Al) == 2
-
-        TM_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_EF_TM[0])[0],
-                                        self.atoms.neighbor_list.get_neighbors(index_EF_TM[1])[0]))
+        self.atoms.update_nl(1.2)
         Al_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_Al[0])[0],
                                         self.atoms.neighbor_list.get_neighbors(index_Al[1])[0]))
-        centering_o = [288] #[[x for x in TM_neigh_list if list(TM_neigh_list).count(x) > 1][0]]
-        self.centering_atom_index = centering_o[0]
+        Al_neigh_list = [x for x in Al_neigh_list if self.atoms[x].symbol == 'O']
+        
+        assert len(index_EF_TM) == 2
+        assert len(index_Al) == 2
         assert len(centering_o) == 1
-        assert self.atoms[centering_o].symbols == 'O'
+        assert len(Al_neigh_list) == 8
 
-        self.EF_indices = list(centering_o)
-        self.EF_indices.extend([value for value in index_EF_TM])
+        return Al_neigh_list + index_Al + index_EF_TM + centering_o
 
-        return np.unique(list(Al_neigh_list) + centering_o + index_Al + index_EF_TM)
+
+if __name__ == '__main__':
+    traj = read('/Users/jiaweiguo/Box/openMM_FF/CHA_minE.traj', ':')
+    for atoms in traj:
+        EFAnalyzer = ExtraFrameworkAnalyzer(atoms)
+        print(EFAnalyzer.get_extraframework_atoms())
+    # view(traj)
