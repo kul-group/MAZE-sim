ff9c249c3e0cb6dd46dc82bc925de4c1925dbce8
Jiawei Guo <jwguo@ucdavis.edu>
2021-08-04 18:36:59 -0700

    Update EFAnalyzer.py

--- scraps/EFAnalyzer.py
+++ scraps/EFAnalyzer.py
@@ -25,20 +25,17 @@ class ExtraFrameworkAnalyzer(object):
         """
         index_EF_TM = [a.index for a in self.atoms if a.symbol in self.TM_list]
         index_Al = [a.index for a in self.atoms if a.symbol == 'Al']
-        
+
         TM_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_EF_TM[0])[0],
                                         self.atoms.neighbor_list.get_neighbors(index_EF_TM[1])[0]))
 
-        # This might be wrong! Not always return desired O index!
-        # works for All CHA structures at least
         centering_o = [[x for x in TM_neigh_list if list(TM_neigh_list).count(x) > 1][0]]
-        # print(centering_o)
-
-        self.atoms.update_nl(1.2)
+        # might not always return the correct atom index
+        
         Al_neigh_list = np.concatenate((self.atoms.neighbor_list.get_neighbors(index_Al[0])[0],
                                         self.atoms.neighbor_list.get_neighbors(index_Al[1])[0]))
         Al_neigh_list = [x for x in Al_neigh_list if self.atoms[x].symbol == 'O']
-        
+
         assert len(index_EF_TM) == 2
         assert len(index_Al) == 2
         assert len(centering_o) == 1
@@ -48,8 +45,19 @@ class ExtraFrameworkAnalyzer(object):
 
 
 if __name__ == '__main__':
-    traj = read('/Users/jiaweiguo/Box/openMM_FF/CHA_minE.traj', ':')
+    # traj = read('/Users/jiaweiguo/Box/openMM_FF/CHA_minE.traj', ':')
+    # traj = read('/Users/jiaweiguo/Box/openMM_FF/AEI_minE.traj', ':')
+    # traj = read('/Users/jiaweiguo/Box/openMM_FF/RHO_minE.traj', ':')
+    traj = read('/Users/jiaweiguo/Box/openMM_FF/MOR_minE.traj', ':')
+
+
+    # view(traj)
+    EF_O_index = []
     for atoms in traj:
         EFAnalyzer = ExtraFrameworkAnalyzer(atoms)
-        print(EFAnalyzer.get_extraframework_atoms())
-    # view(traj)
+        EF_O_index.append(EFAnalyzer.get_extraframework_atoms()[-1])
+    print(np.unique(EF_O_index))
+    print(EF_O_index.count(96))
+    print(EF_O_index.count(33))
+    # index check, if len(np.unique(EF_O_index)) > 1, remove
+    
